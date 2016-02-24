052c4ac4c876f2fcf7881abe679874f320c76c56
Christian Robert <dev@perdian.de>
2016-02-24 21:37:13 +0100

    Invoke autojump directly if no on a Mac system

--- zsh/zshrc.d/autojump.zsh
+++ zsh/zshrc.d/autojump.zsh
@@ -1,4 +1,8 @@
 # Enable Autojump
 if which brew >/dev/null; then
     [[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
+else
+    if [ -e /usr/share/autojump/autojump.sh ] ; then
+        . /usr/share/autojump/autojump.zsh
+    fi
 fi
