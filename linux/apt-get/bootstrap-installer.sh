72009f414afd04b583ae9677417cd5699163a29b
Christian Robert <dev@perdian.de>
2016-02-24 21:27:33 +0100

    Add installer for Linux systems

---
+++ linux/apt-get/bootstrap-installer.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if which apt-get >/dev/null; then
+
+    sudo apt-get install git nano zsh coreutils wget autojump software-properties-common python-software-properties realpath
+
+    ALL_APT_GET_REPOSITORIES=$(apt-cache policy)
+    if [[ ! "${ALL_APT_GET_REPOSITORIES}" =~ webupd8team/java ]]; then
+        sudo add-apt-repository ppa:webupd8team/java
+        sudo apt-get update
+    fi
+
+    sudo apt-get install oracle-java8-installer
+    sudo apt-get install oracle-java8-set-default
+
+fi
