f41b567951f1c50628029208df16475a8137e691
Christian Robert <dev@perdian.de>
2016-03-01 13:47:57 +0100

    Allow for .symlink files to be located also in subdirectories of the current user profile directory. Also add environments.

--- README.md
+++ README.md
@@ -10,15 +10,47 @@ As with many projects I'm standing on the shoulders of great people and have
 reclessly copied ideas and configurations from many sources, most prominently
 [Zach Holman](https://github.com/holman/dotfiles) and [Ryan Bates](https://github.com/ryanb/dotfiles).
 
+## Environments
+
+I do not want all dotfiles to be available on all environments. For example
+certain configurations only apply to my personal machine (that is running OS X)
+and not my server (that is running Ubuntu).
+
+The concept of *environments* provides an abstraction for this where during the
+bootstrap a series of discovery processes is started and only the environments
+discovered will be made available. Environments can be combined which means that
+a single installation of the dotfiles can include multiple environments.
+
+The `default` environment is special in that it will be included into each and
+every bootstrap process and does not have to be defined explicitely.
+
+Apart from that there are two major other types of environments: `automatic`
+and `manual`. All the environments defined in the `automatic` directory will
+be selected automatically if certain conditions are detected by the
+`bootstrap.py` script. For example the `osx` environment is activated if the
+script detects that it is running on a OS X machine. All the environments
+defined in the `manual` directory will have to be activated by adding them to
+the `bootstrap.py` call on the command line:
+
+    $ environments/bootstrap.py env1 env2 ...
+
 ## Topics
 
-The individual configuration files and resources have been grouped around
-*topic areas*, where each topic area is represented by its own subdirectory
-inside the main repository.
+The individual configuration files and resources inside the environments are
+grouped around *topic areas*, where each topic area is represented by its own
+subdirectory inside the environment directory.
 
 Anything with an extension of `.zsh` will get automatically included into your
-shell. Anything with an extension of `.symlink` will get symlinked without
-extension into `$HOME` when you run `bootstrap.py`.
+zsh shell. Anything an extension of `.symlink` will get symlinked into a target
+directory inside the home directory (or the home directory itself). The part
+between an 'at' character and the `.symlink` extension will determine the exact
+target directory inside the home directory. If no explicit target directory is
+given (the extension is just `.symlink`) then the link will be created inside
+the users home directory (`$HOME`). Otherwise the part between he 'at' character
+and the `.symlink` extension will be translated into the actual directory inside
+the home directory, where all at characters will replaced by a forward slash.
+So for example the file `private.xml@Application Support@karabiner.symlink` will
+be translated to the actual target directory `~/Library/Application Support/karabiner`.
 
 ## What's inside
 
@@ -30,19 +62,22 @@ don't use, and build on what you do use.
 
 ## Components
 
-There are a few special files in the hierarchy.
+There are a few special files inside the hierarchy.
 
-- **bin/**: Anything in `bin/` will get added to your `$PATH` and be made
-  available everywhere.
-- **topic/\*.zsh**: Any files ending in `.zsh` get loaded into your environment.
-- **topic/\*.symlink**: Any files ending in `.symlink` get symlinked into
-  your `$HOME`. This enabled you to keep all of your files versioned in your
-  dotfiles but still keep those autoloaded files in your home directory. These
-  get symlinked in when you run `bootstrap.py`.
+- **environments/auto/bin/**: Anything in `bin/` will get added to your `$PATH`
+  and will be made available everywhere.
+- **environments/<environment>/topic/\*.zsh**: Any files ending in `.zsh` get
+  loaded into your environment.
+- **environments/<environment>/topic/\*.symlink: Any files ending in `.symlink`
+  will get symlinked into your `$HOME` directory - or a subdirectory (see
+  above). This enables you to keep all of your files versioned in your dotfiles
+  but still keep those autoloaded files in your home directory. The symlinks
+  will be created when running the `bootstrap.py` script inside `environments`.
 
 ## Prerequesits
 
-There are only a few prerequesites that you need to have in place to use the dotfiles:
+There are only a few prerequesites that you need to have in place to use the
+dotfiles:
 
 ### OS X
 
---
+++ environments/bootstrap.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python
+import os
+import subprocess
+import glob
+import shutil
+import sys
+import fnmatch
+
+# Find all the relevant environments that should be activated
+activeEnvironments = [ "default" ];
+for arg in sys.argv[1:]:
+    activeEnvironments.append("manual/" + arg);
+if (sys.platform == "darwin"):
+    activeEnvironments.append("auto/osx");
+elif (sys.platform == "linux" or sys.platform == "linux2"):
+    activeEnvironments.append("auto/linux");
+print "Resolved active environments: " + str(activeEnvironments);
+
+# Store the list of environments into the .dotfiles-environments file inside the
+# user home directory
+environmentConfigurationFileContent = "";
+for activeEnvironment in activeEnvironments:
+    environmentConfigurationFileContent += activeEnvironment + "\n";
+environmentConfigurationFileName = os.path.join(os.path.expanduser("~"), ".dotfiles-environments");
+environmentConfigurationFile = open(environmentConfigurationFileName, "w");
+environmentConfigurationFile.write(environmentConfigurationFileContent);
+environmentConfigurationFile.close();
+
+environmentsRootDirectory = os.path.dirname(os.path.abspath(__file__))
+print "Bootstrapping dotfiles installation from root directory: " + environmentsRootDirectory
+
+# Resolve all the .symlink files (or directories for that matter) and make sure that they are
+# correctly linked into the $HOME directory (or a child directory inside the $HOME directory)
+globalErrorHandlingMode = None;
+for activeEnvironment in activeEnvironments:
+    activeEnvironmentDirectory = os.path.join(environmentsRootDirectory, activeEnvironment);
+    if os.path.exists(activeEnvironmentDirectory):
+
+        print "Bootstrapping dotfiles from environment directory: " + activeEnvironmentDirectory;
+        for root, dirNames, fileNames in os.walk(activeEnvironmentDirectory):
+            for sourceFileName in fnmatch.filter(fileNames + dirNames, '*.symlink'):
+
+                sourceFile = os.path.join(root, sourceFileName)
+                sourceFileResolved = os.path.realpath(sourceFile)
+                sourceFileName = os.path.basename(sourceFile)
+
+                sourceFileIndexOfSymlink = sourceFileName.find(".symlink");
+                sourceFileIndexOfTargetDirectoryName = sourceFileName.find("@");
+
+                if sourceFileIndexOfTargetDirectoryName > -1:
+                    targetDirectoryEncoded = sourceFileName[sourceFileIndexOfTargetDirectoryName+1:sourceFileIndexOfSymlink];
+                    targetDirectoryName = targetDirectoryEncoded.replace("@", "/");
+                    targetDirectory = os.path.join(os.path.expanduser("~"), targetDirectoryName);
+                    targetFileName = sourceFileName[0:sourceFileIndexOfTargetDirectoryName];
+                else:
+                    targetDirectory = os.path.expanduser("~");
+                    targetFileName = sourceFileName[0:sourceFileIndexOfSymlink];
+
+                targetFile = os.path.join(targetDirectory, targetFileName);
+                targetFileResolved = os.path.realpath(targetFile)
+
+                if os.path.exists(targetFileResolved) and sourceFileResolved == targetFileResolved:
+
+                    # The source file exists and already represents a link to the target
+                    # file so there is nothing that we need to do.
+                    print "Target '" + str(targetFile) + " already symlinked to '" + str(sourceFileResolved) + "'"
+                    pass
+
+                elif not os.path.exists(targetFileResolved):
+
+                    if os.path.islink(targetFile):
+                        os.unlink(targetFile)
+
+                    targetFileParent = os.path.abspath(os.path.join(targetFileResolved, os.pardir));
+                    if not os.path.exists(targetFileParent):
+                        os.makedirs(targetFileParent);
+
+                    os.symlink(sourceFile, targetFile)
+                    print "Symlinked '" + str(targetFile) + "' to '" + str(sourceFileResolved) + "'"
+
+                else:
+                    # The target file already exists and is not identifical with the file
+                    # that we would like to place there. So ask the user what to do
+                    if globalErrorHandlingMode == None:
+                        print "Target file '" + str(targetFile) + "' is already existing. What do you want to do?"
+                        print "[s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
+                        errorHandlingModeResult = raw_input(">> ")
+                        if len(errorHandlingModeResult) > 0 and errorHandlingModeResult == errorHandlingModeResult.upper():
+                            globalErrorHandlingMode = errorHandlingModeResult.lower()
+                        errorHandlingMode = errorHandlingModeResult.lower()
+                    else:
+                        errorHandlingMode = globalErrorHandlingMode
+
+                    if errorHandlingMode == "s" or errorHandlingMode == "":
+                        pass
+                    elif errorHandlingMode == "o":
+                        if os.path.isdir(targetFile):
+                            shutil.rmtree(targetFile)
+                        elif os.path.isfile(targetFile):
+                            os.remove(targetFile)
+                        os.symlink(sourceFile, targetFile)
+                        print "Symlinked '" + str(targetFile) + "' to '" + str(sourceFileResolved) + "'"
+                    elif errorHandlingMode == 'b':
+                        backupFile = targetFile + ".backup"
+                        print "Moving '" + str(targetFile) + "' to '" + str(backupFile) + "'"
+                        shutil.move(targetFile, backupFile)
+                        os.symlink(sourceFile, targetFile)
+                        print "Symlinked '" + str(targetFile) + "' to '" + str(sourceFileResolved) + "'"
+
+        for root, dirNames, fileNames in os.walk(activeEnvironmentDirectory):
+            for bootstrapInstallerFileName in sorted(fnmatch.filter(fileNames, 'bootstrap-installer.*')):
+                bootstrapInstallerFile = os.path.join(root, bootstrapInstallerFileName)
+
+                print "Executing installer at: " + str(bootstrapInstallerFile)
+                subprocess.call(["chmod", "a+x", str(bootstrapInstallerFile)])
+                subprocess.call(str(bootstrapInstallerFile))
