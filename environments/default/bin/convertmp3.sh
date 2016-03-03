9e3c9fa2fd99a61efd50a58563c58154d9d3fccf
Christian Robert <dev@perdian.de>
2016-03-03 21:27:43 +0100

    Use double brackets consistently

--- environments/default/bin/convertmp3.sh
+++ environments/default/bin/convertmp3.sh
@@ -1,6 +1,6 @@
 for fileName in *
 do
-  if [ -f "$fileName" ] ; then
+  if [[ -f "$fileName" ]] ; then
     ffmpeg -i "$fileName" -ab 128k "$fileName.mp3"
     echo "Finished converting $fileName"
   fi
--- environments/default/zsh/bootstrap-installer.sh
+++ environments/default/zsh/bootstrap-installer.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-if [ ! -d $HOME/.oh-my-zsh ] ; then
+if [[ ! -d $HOME/.oh-my-zsh ]] ; then
 
     echo "Starting installation of Oh My ZSH"
     git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
     echo "Completed installation of Oh My ZSH"
 
     TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
-    if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
+    if [[ "$TEST_CURRENT_SHELL" != "zsh" ]]; then
         if hash chsh >/dev/null 2>&1; then
             chsh -s $(grep /zsh$ /etc/shells | tail -1)
         else
