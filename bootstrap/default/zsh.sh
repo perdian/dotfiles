6fbb402436c357e841ae2c6d801d0383084261e9
Christian Robert <dev@perdian.de>
2018-09-05 16:12:47 +0200

    Check for existence of zsh at every startup

--- bootstrap/default/zsh.sh
+++ bootstrap/default/zsh.sh
@@ -1,13 +1,3 @@
-#!/bin/sh
-if [ ! -d $HOME/.oh-my-zsh ] ; then
-    echo "Installing Oh My ZSH"
-    git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
-fi
-if [ ! -d $HOME/.oh-my-zsh/custom/themes/powerlevel9k ] ; then
-    echo "Installing powerlevel9k theme for Oh My ZSH"
-    git clone --depth=1 https://github.com/bhilburn/powerlevel9k.git $HOME/.oh-my-zsh/custom/themes/powerlevel9k
-fi
-
 TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
 if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
     if hash chsh >/dev/null 2>&1; then
