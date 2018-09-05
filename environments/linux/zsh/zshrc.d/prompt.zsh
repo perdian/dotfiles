d87192776db8e150b186d4127355681b0ab27ca8
Christian Robert <dev@perdian.de>
2018-09-05 14:21:24 +0200

    Remove old prompt configuration

--- environments/linux/zsh/zshrc.d/prompt.zsh
+++
@@ -1,6 +0,0 @@
-local user_info="%n@%m"
-local ret_status="%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ %s)%{$fg_bold[green]%}"
-local directory_status="%{$fg[cyan]%}%d"
-local end_status="%{$fg_bold[blue]%}%#"
-
-PROMPT='${user_info} ${ret_status} ${directory_status} %{$fg_bold[blue]%}$(git_prompt_info) ${end_status} %{$reset_color%}'
