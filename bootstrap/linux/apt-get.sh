ccecf4c7b4e3d8f7eec5fe54916cf6ff122c68e5
Christian Robert <dev@perdian.de>
2018-09-05 14:34:11 +0200

    Remove no longer used packages

--- bootstrap/linux/apt-get.sh
+++ bootstrap/linux/apt-get.sh
@@ -1,19 +1,4 @@
 #!/bin/sh
 if which apt-get >/dev/null; then
-
     sudo apt-get --yes install dialog git vim nano zsh coreutils wget autojump software-properties-common python-software-properties realpath
-
-    ALL_APT_GET_REPOSITORIES=$(apt-cache policy)
-    if [[ ! "${ALL_APT_GET_REPOSITORIES}" =~ webupd8team/java ]]; then
-        sudo add-apt-repository ppa:webupd8team/java
-        sudo apt-get --yes update
-    fi
-    if [[ ! "${ALL_APT_GET_REPOSITORIES}" =~ brightbox/ruby-ng ]]; then
-        sudo add-apt-repository ppa:brightbox/ruby-ng
-        sudo apt-get --yes update
-    fi
-
-    sudo apt-get install --yes oracle-java8-installer
-    sudo apt-get install --yes oracle-java8-set-default maven ant ruby2.2 ruby2.2-dev
-
 fi
