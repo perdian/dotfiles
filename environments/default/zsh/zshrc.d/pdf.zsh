84f31e7a0a0e4567c9897fcc7f65b8e02ac9d406
Christian Robert <dev@perdian.de>
2018-10-13 11:54:52 +0200

    Add PDF compression function

---
+++ environments/default/zsh/zshrc.d/pdf.zsh
@@ -0,0 +1,10 @@
+# https://blog.omgmog.net/post/compressing-pdf-from-your-mac-or-linux-terminal-with-ghostscript/
+# -dPDFSETTINGS=/screen   (screen-view-only quality, 72 dpi images)
+# -dPDFSETTINGS=/ebook    (low quality, 150 dpi images)
+# -dPDFSETTINGS=/printer  (high quality, 300 dpi images)
+# -dPDFSETTINGS=/prepress (high quality, color preserving, 300 dpi imgs)
+# -dPDFSETTINGS=/default  (almost identical to /screen)
+
+pdfcompress() {
+    gs -sDEVICE=pdfwrite -dNOPAUSE -dQUIET -dBATCH -dPDFSETTINGS=/${3:-"ebook"} -dCompatibilityLevel=1.5 -sOutputFile="$2" "$1"
+}
