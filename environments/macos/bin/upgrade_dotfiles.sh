1ecd4b016eae9fb89f8bfd7ba490e3b959f7e620
Christian Robert <dev@perdian.de>
2018-11-01 15:54:02 +0100

    Move upgrade script into separate file

---
+++ environments/macos/bin/upgrade_dotfiles.sh
@@ -0,0 +1,14 @@
+#!/usr/local/bin/zsh
+echo "Updating Homebrew"
+brew update && brew upgrade
+
+echo "Updating Oh My Zsh"
+upgrade_oh_my_zsh
+
+DOTFILES_HOME_RAW="$(dirname $0)/../../../"
+DOTFILES_HOME="$(realpath $DOTFILES_HOME_RAW)"
+echo "Updating dotfiles in $DOTFILES_HOME"
+cd $DOTFILES_HOME
+git pull
+$DOTFILES_HOME/install.py $(< ~/.dotfiles-environments)
+source ~/.zshrc
--- environments/macos/zsh/zshrc.d/up.zsh
+++
@@ -1,6 +0,0 @@
-up() {
-    echo "Updating Homebrew"
-    brew update && brew upgrade
-    echo "Updating Oh My Zsh"
-    upgrade_oh_my_zsh
-}
