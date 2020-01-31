432930208cbb7893e502461b968399611837dd70
Christian Seifert <dev@perdian.de>
2020-01-31 12:02:12 +0100

    Optimize upgrade script

--- environments/macos/bin/upgrade_dotfiles.sh
+++ environments/macos/bin/upgrade_dotfiles.sh
@@ -1,15 +1,25 @@
 #!/usr/local/bin/zsh
-echo "Updating Homebrew"
+echo "Upgrading Homebrew"
 brew update && brew upgrade && brew cleanup
-
-echo "Updating Oh My Zsh"
-source ~/.oh-my-zsh/oh-my-zsh.sh
-upgrade_oh_my_zsh
 
 DOTFILES_HOME_RAW="$(dirname $0)/../../../"
 DOTFILES_HOME="$(realpath $DOTFILES_HOME_RAW)"
-echo "Updating dotfiles in $DOTFILES_HOME"
-cd $DOTFILES_HOME
-git pull
+echo "Upgrading dotfiles repository in $DOTFILES_HOME"
+cd $DOTFILES_HOME && git pull
+
+echo "Upgrading Oh My Zsh"
+source ~/.oh-my-zsh/oh-my-zsh.sh
+upgrade_oh_my_zsh
+
+echo "Upgrading Oh My Zsh resources"
+for custom_directory in ~/.oh-my-zsh/custom/*/*
+do
+  if [ -d "${custom_directory}/.git" ]; then
+    echo "Upgrading Oh My Zsh custom entry at: ${custom_directory}"
+    cd ${custom_directory}/ && git pull
+  fi
+done
+
+echo "Applying latest dotfiles configuration"
 $DOTFILES_HOME/install.py $(< ~/.dotfiles-environments)
 source ~/.zshrc
