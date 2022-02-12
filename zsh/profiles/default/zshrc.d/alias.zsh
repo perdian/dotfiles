e7664d931fb2591b56d64f5c8f97775aea15e690
Christian Seifert <dev@perdian.de>
2022-02-12 23:32:50 +0100

    Replace youtubedownloader

--- zsh/profiles/default/zshrc.d/alias.zsh
+++ zsh/profiles/default/zshrc.d/alias.zsh
@@ -15,4 +15,4 @@ alias ls="ls -G"
 
 alias k="kubectl"
 
-alias ytv="youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best'"
+alias ytv="yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best'"
