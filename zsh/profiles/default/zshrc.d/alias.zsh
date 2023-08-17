c8c5ad0e5556b654c271db36e3813e72dbd60070
Christian Seifert <dev@perdian.de>
2023-08-17 19:26:37 +0200

    Add ytvs for "small" video

--- zsh/profiles/default/zshrc.d/alias.zsh
+++ zsh/profiles/default/zshrc.d/alias.zsh
@@ -15,5 +15,6 @@ alias ls="ls -G"
 
 alias k="kubectl"
 
-alias ytv="yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' -o '%(title)s.%(ext)s'"
+alias ytv="yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' -o '%(title)s.%(ext)s'"
+alias ytvs="yt-dlp -f 'bestvideo[ext=mp4][height<=720]+bestaudio[ext=m4a]/mp4' -o '%(title)s.%(ext)s'"
 alias yta="yt-dlp -x --audio-format mp3 --audio-quality 0 -o '%(title)s.%(ext)s'"
