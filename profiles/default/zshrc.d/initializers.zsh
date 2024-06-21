8a56f5db5193501bfcd0d7c5bb5e226d7fde70da
Christian Seifert <dev@perdian.de>
2024-06-21 21:32:58 +0200

    Add Angular completion

--- profiles/default/zshrc.d/initializers.zsh
+++ profiles/default/zshrc.d/initializers.zsh
@@ -1,3 +1,4 @@
 [[ -x "$(command -v direnv)" ]] && eval "$(direnv hook zsh)"
 [[ -x "$(command -v rbenv)" ]] && eval "$(rbenv init -)"
 [[ -x "$(command -v zoxide)" ]] && eval "$(zoxide init zsh)"
+[[ -x "$(command -v ng)" ]] && eval "$(ng completion script)"
