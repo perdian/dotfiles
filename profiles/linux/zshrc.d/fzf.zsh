ed9c202a0177353c452f4961b08065d3f171e1fd
Christian Seifert <dev@perdian.de>
2026-01-30 10:35:39 +0100

    Always set correct path to fzf

--- profiles/linux/zshrc.d/fzf.zsh
+++ profiles/linux/zshrc.d/fzf.zsh
@@ -1,6 +1,6 @@
 if [[ ! -d "${DOTFILES_DATA}/fzf/" ]]; then
   git clone --depth 1 https://github.com/junegunn/fzf.git "${DOTFILES_DATA}/fzf/"
   ${DOTFILES_DATA}/fzf/install --bin --key-bindings --completion --no-update-rc --no-zsh --no-bash
-  PATH="${DOTFILES_DATA}/fzf/bin:$PATH"
 fi
+PATH="${DOTFILES_DATA}/fzf/bin:$PATH"
 source <(${DOTFILES_DATA}/fzf/bin/fzf --zsh)
