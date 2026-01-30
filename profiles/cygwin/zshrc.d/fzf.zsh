5366b5d2c03f4556c3e58f9a760c9683827bfb45
Christian Seifert <dev@perdian.de>
2026-01-30 08:57:22 +0100

    Add fzf

---
+++ profiles/cygwin/zshrc.d/fzf.zsh
@@ -0,0 +1,8 @@
+if [[ ! -d "${DOTFILES_DATA}/fzf/" ]]; then
+  git clone --depth 1 https://github.com/junegunn/fzf.git "${DOTFILES_DATA}/fzf/"
+  ${DOTFILES_DATA}/fzf/install --bin --key-bindings --completion --no-update-rc --no-zsh --no-bash
+fi
+PATH="${DOTFILES_DATA}/fzf/bin:$PATH"
+
+export MSYS=enable_pcon # https://github.com/junegunn/fzf/issues/4106
+source <(${DOTFILES_DATA}/fzf/bin/fzf.exe --zsh)
